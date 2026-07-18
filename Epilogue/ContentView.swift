cc26a06f63f320a6c896bd6551aeb91a43740fd3
zehra öner <95606849+zzehraooner@users.noreply.github.com>
2026-07-18 23:32:29 +0300

    Initial Commit

---
+++ Epilogue/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Epilogue
+//
+//  Created by Zehra Öner on 18.07.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Epilogue/EpilogueApp.swift
@@ -0,0 +1,17 @@
+//
+//  EpilogueApp.swift
+//  Epilogue
+//
+//  Created by Zehra Öner on 18.07.2026.
+//
+
+import SwiftUI
+
+@main
+struct EpilogueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
